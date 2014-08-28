c13d95d7c48c2967b913aed7243a3de38ad638a5
James <seeyou8286@163.com>
2014-08-29 01:39:47 +0800

    Add

---
+++ July/src/test/java/Singapore/Maven/Tutorial/July/AnnotationTest.java
@@ -0,0 +1,14 @@
+package Singapore.Maven.Tutorial.July;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnnotationTest {
+	
+	@Test
+	public void testAnno(){
+		assertTrue( true );
+	}
+
+}
